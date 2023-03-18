4e6ba3430984484553fd67120c7f8da8a8ce7f36
Andras Ferenczi <andrasf@hotmail.com>
2023-03-17 19:14:44 -0700

    fixed mnist data loader

--- blocklearning/training_algos/regular_algo.py
+++ blocklearning/training_algos/regular_algo.py
@@ -1,45 +1,41 @@
 
-from tensorflow.keras.optimizers.legacy import SGD
-from .base_algo import BaseAlgo
-import tensorflow as tf
-from sklearn.metrics import accuracy_score
-import tensorflow_privacy
 from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy
+from tensorflow_privacy.DPKerasSGDOptimizer import DPKerasSGDOptimizer
+from .base_algo import BaseAlgo
+from sklearn.metrics import accuracy_score
+import tensorflow as tf
 
 class RegularAlgo(BaseAlgo):
 
-    def __init__(self, model, epochs=1, image_lib = 'mnist', batch_size = 30, verbose=False):
+    def __init__(self, model, epochs=1, image_lib='mnist', batch_size=30, verbose=False):
         super().__init__(model, epochs, verbose)
         l2_norm_clip = 1.5
         noise_multiplier = 1.3
         num_microbatches = 30
         learning_rate = 0.25
 
-        if batch_size % num_microbatches != 0:
+        if batch_size % num_microbatches:
             raise ValueError('Batch size should be an integer multiple of the number of microbatches')
 
-
-        # Select your differentially private optimizer
-        optimizer = tensorflow_privacy.DPKerasSGDOptimizer(
+        optimizer = DPKerasSGDOptimizer(
             l2_norm_clip=l2_norm_clip,
             noise_multiplier=noise_multiplier,
             num_microbatches=num_microbatches,
             learning_rate=learning_rate)
 
-        # Select your loss function
-        loss = tf.keras.losses.CategoricalCrossentropy(
-            from_logits=True, reduction=tf.losses.Reduction.NONE)
-        
-        self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+        self.model.compile(optimizer=optimizer, 
+                           loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.losses.Reduction.NONE), 
+                           metrics=['accuracy'])
 
     def __freeze_except_last(self):
         idx = 2 if self.image_lib == 'cifar' else 3
-        for layer in self.model.layers[:-idx]:
-            layer.trainable=False
+        stop_index = -idx
+        for layer in self.model.layers[:stop_index]:
+            layer.trainable = False
         
 
-    def fit(self, batched_ds, freeze_except_last = False):
-        if  freeze_except_last:
+    def fit(self, batched_ds, freeze_except_last=False):
+        if freeze_except_last:
             self.__freeze_except_last()
 
         return self.model.fit(batched_ds, epochs=self.epochs, verbose=self.verbose)
@@ -57,10 +53,10 @@ class RegularAlgo(BaseAlgo):
     def set_model(self, model):
         self.model = model
 
-    def set_weights(self, weights, freeze_except_last = False):
+    def set_weights(self, weights, freeze_except_last=False):
         self.model.set_weights(weights)
 
-        if  freeze_except_last:
+        if freeze_except_last:
             self.__freeze_except_last()
 
     def get_weights(self):
--- testbed/mnist_data_loader.py
+++ testbed/mnist_data_loader.py
@@ -7,86 +7,66 @@ import tensorflow as tf
 from imutils import paths
 import random
 import click
+from sklearn.utils import shuffle
 
 def load(paths, verbose=-1):
-    '''expects images for each class in seperate dir, 
-    e.g all digits in 0 class in the directory named 0 '''
-    data = list()
-    labels = list()
-    # loop over the input images
-    for (i, imgpath) in enumerate(paths):
-        # load the image and extract the class labels        
-        im_gray = cv2.imread(imgpath , cv2.IMREAD_GRAYSCALE)
-        image = np.array(im_gray).flatten() # cv2.imread(imgpath) 
-        # print(image.shape)
-        label = imgpath.split(os.path.sep)[-2]
-        # scale the image to [0, 1] and add to list
-        data.append(image/255)
-        labels.append(label)
-        # show an update every `verbose` images
-        if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
-            print("[INFO] processed {}/{}".format(i + 1, len(paths)))
-    return data, labels
+    train, test = tf.keras.datasets.mnist.load_data()
+    train_data, train_labels = train
+    test_data, test_labels = test
 
-def batch_data(data_shard, bs=5):
-    '''Takes in a clients data shard and create a tfds object off it
-    args:
-        shard: a data, label constituting a client's data shard
-        bs:batch size
-    return:
-        tfds object'''
+    train_data = np.array(train_data, dtype=np.float32) / 255
+    test_data = np.array(test_data, dtype=np.float32) / 255
+
+    train_data = train_data.reshape(train_data.shape[0], 28*28)
+    test_data = test_data.reshape(test_data.shape[0], 28*28)
+
+    train_labels = np.array(train_labels, dtype=np.int32)
+    test_labels = np.array(test_labels, dtype=np.int32)
+
+    train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=10)
+    test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=10)
+
+    assert train_data.min() == 0.
+    assert train_data.max() == 1.
+    assert test_data.min() == 0.
+    assert test_data.max() == 1.
+
+    return train_data, test_data, train_labels, test_labels
+
+def batch_data(data_shard, bs=30, flip=False):
     #seperate shard into data and labels lists
     data, label = zip(*data_shard)
+    data = np.array(data)
+    
+    labels = list(label)
+    
+    if flip:      
+        labels = shuffle(labels)
+    
+    labels = np.array(labels)
+    data = np.array(list(data))
+    
+    assert(data.shape[0] == labels.shape[0])
+    
+    dataset = tf.data.Dataset.from_tensor_slices((data, labels))
+    
     X_data_train, X_data_test, y_data_train, y_data_test = train_test_split(data, 
                                                     label, 
-                                                    train_size=0.8,
-                                                    test_size=0.2, 
+                                                    train_size=0.9,
+                                                    test_size=0.1, 
                                                     random_state=42)
-    train_dataset = tf.data.Dataset.from_tensor_slices((list(X_data_train), list(y_data_train)))
-    test_dataset = tf.data.Dataset.from_tensor_slices((list(X_data_test), list(y_data_test)))
+        
+    train_dataset = tf.data.Dataset.from_tensor_slices((np.array(list(X_data_train)), np.array(list(y_data_train))))
+    test_dataset = tf.data.Dataset.from_tensor_slices((np.array(list(X_data_test)), np.array(list(y_data_test))))
     return (train_dataset.shuffle(len(y_data_train)).batch(bs), test_dataset.batch(bs))
 
-def create_clients(image_list, label_list, num_clients=10):
-    ''' return: a dictionary with keys clients' names and value as 
-                data shards - tuple of images and label lists.
-        args: 
-            image_list: a list of numpy arrays of training images
-            label_list:a list of binarized labels for each image
-            num_client: number of fedrated members (clients)
-            initials: the clients'name prefix, e.g, clients_1 
-            
-    '''
-
+def create_clients(image_list, label_list, num_clients=100, initial='clients'):
     #create a list of client names
-    client_names = ['{}'.format(i) for i in range(num_clients)]
-
-    #randomize the data
+    client_names = ['{}_{}'.format(initial, i+1) for i in range(num_clients)]
     data = list(zip(image_list, label_list))
-    random.shuffle(data)  # <- IID
-    
-    # sort data for non-iid
-    max_y = np.argmax(label_list, axis=-1)
-    sorted_zip = sorted(zip(max_y, label_list, image_list), key=lambda x: x[0])
-    data = [(x,y) for _,y,x in sorted_zip]
-
-    #shard data and place at each client
-    # size = len(data)//num_clients
-    # shards = [data[i:i + size] for i in range(0, size*num_clients, size)]
-
-    shards = []
-    vals = np.random.default_rng().dirichlet(np.ones(num_clients), size=1)
-    k_nums = [round(v) for v in vals[0]*len(data)]
-    offset = 0
-
-    for i in range(num_clients):
-        end = len(data) if i == num_clients - 1 else offset + k_nums[i]
-        shards.append(data[offset : end])
-        offset = end + 1
-
-    #number of clients must equal number of shards
+    size = len(data)//num_clients   
+    shards = [data[i:i + size] for i in range(0, size*num_clients, size)]
     assert(len(shards) == len(client_names))
-    assert(sum([len(shard) for shard in shards]) <= len(data))
-
     return {client_names[i] : shards[i] for i in range(len(client_names))} 
 
 @click.command()
@@ -97,7 +77,7 @@ def main(no_trainers, data_path, img_path):
     no_trainers = int(no_trainers)
     image_paths = list(paths.list_images(img_path))
 
-    image_list, label_list = load(image_paths, verbose=10000)
+    image_list, _, label_list, _ = load(image_paths, verbose=10000)
 
     lb = LabelBinarizer()
     label_list = lb.fit_transform(label_list)
@@ -119,10 +99,5 @@ def main(no_trainers, data_path, img_path):
             tf.data.experimental.save(test_ds, path_template.format(data_path, no_trainers, 'test', client_name))
         else:
             tf.data.experimental.save(test_ds, "{}/mnist/{}/owner_val.tfrecord".format(data_path, no_trainers))
-
-    # train_ds = tf.data.experimental.load(path_template.format(NO_TRAINERS, 'train', '1'))
-    # print(train_ds)
-
-   
 
 main()
